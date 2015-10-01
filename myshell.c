53f5478369f7f508a5c3edd1a14d20310b6be3c5
Noah Butler <NoahButler@Stephens-iMac.local>
2015-09-30 19:53:29 -0500

    fixed execute_pipe

--- myshell.c
+++ myshell.c
@@ -185,7 +185,7 @@ int do_command(char **args, int in, int out) {
         //     dup2(in, 0); 
         //     close(in);
         // }
-
+        printf("in: %d, out: %d, append: %d \n", input, output, append);
         if(input)
             freopen(input_filename, "r", stdin);
         if(output)
